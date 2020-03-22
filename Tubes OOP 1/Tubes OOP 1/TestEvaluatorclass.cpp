b8811c02bc5af19234ab89e2d8338b4f5421a1e2
stefanusgusega <gunawanstefanus006@gmail.com>
2020-03-23 01:02:50 +0700

    bntr

--- Tubes OOP 1/Tubes OOP 1/TestEvaluatorclass.cpp
+++ Tubes OOP 1/Tubes OOP 1/TestEvaluatorclass.cpp
@@ -5,8 +5,16 @@ using namespace std;
 
 
 int main(int argc, char** args){
-    Evaluator<double,double> Ev;
-    string exp = args[1];
-    float S = Ev.solveExpression(exp);
-    cout << S << endl;
+    try {
+        Evaluator<double,double> Ev;
+    // string exp = args[2];
+    // float S = Ev.solveExpression(exp);
+    // cout << S << endl;
+        string exp = "2+----4";
+        float S = Ev.solveExpression(exp);
+        cout << S << endl;
+    }
+    catch (BaseError* err){
+        cout << err->getMessage() << endl;
+    }
 }
